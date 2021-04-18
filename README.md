e6027343986ad4cab7de9f71425a2a614ea1d400
tony <ayakovlenko@users.noreply.github.com>
2021-04-18 03:31:30 +0300

    docs: update example

--- README.md
+++ README.md
@@ -9,10 +9,13 @@ deno install -f --allow-read main.ts
 # run example
 $ wf ./examples/templates/daily.ts
 
-[
-  {
-    "text": "2021-04-18",
-    "items": []
-  }
-]
+<?xml version="1.0"?>
+<opml version="2.0">
+  <body>
+    <outline text="2021-04-18" _note="Sunday">
+      <outline text="TO DO"></outline>
+      <outline text="DONE"></outline>
+    </outline>
+  </body>
+</opml>
 ```
--- examples/templates/daily.ts
+++ examples/templates/daily.ts
@@ -1,8 +1,31 @@
-import { Item } from "https://raw.githubusercontent.com/ayakovlenko/wf/0.0.1/mod.ts";
+import { Item } from "https://raw.githubusercontent.com/ayakovlenko/wf/0.0.2/lib.ts";
 import { format } from "https://deno.land/std@0.93.0/datetime/mod.ts";
 
-const template: Item | Item[] = [
-  new Item(format(new Date(), "yyyy-MM-dd")),
+const weekday = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const d = new Date();
+
+const template: Item[] = [
+  {
+    text: format(d, "yyyy-MM-dd"),
+    note: weekday[d.getDay()],
+    items: [
+      {
+        text: "TO DO",
+      },
+      {
+        text: "DONE",
+      },
+    ],
+  },
 ];
 
 export default template;
